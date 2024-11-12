10e592f45ec8b4784341f905dbee99b3c31a1a2b
netizenews <netizenewsmedia@gmail.com>
2024-11-12 17:42:09 +0700

    Update shopee1.js

--- shopee1.js
+++ shopee1.js
@@ -8,17 +8,30 @@ function addEvent(obj, eventName, func) {
     }
 }
 
-addEvent(window, "load", function(e) {
-    addEvent(document.body, "click", function(e) {
-        if (document.cookie.indexOf("bkc=lyk") == -1) {
-            var params = 'width=' + screen.width;
-            params += ', height=' + screen.height;
-            params += ', top=1000, left=1200px, scrollbars=no';
-            params += ', fullscreen=yes, width=1000, height=800';
-            
-            var w = window.open("https://s.shopee.co.id/3Aq7OcqIox", 'window', params).blur();
-            document.cookie = "bkc=lykshoptinhoc";
-            window.focus();
-        }
-    });
+function setCookie(name, value, hours) {
+    const date = new Date();
+    date.setTime(date.getTime() + (hours * 60 * 60 * 1000)); // Menambahkan waktu kedaluwarsa dalam milidetik
+    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+}
+
+addEvent(window, "load", function() {
+    setTimeout(function() {
+        addEvent(document.body, "click", function() {
+            // Memeriksa apakah cookie sudah ada
+            if (document.cookie.indexOf("bkc=lyk") === -1) {
+                var params = 'width=' + screen.width;
+                params += ', height=' + screen.height;
+                params += ', top=1000, left=1200px, scrollbars=no';
+                params += ', fullscreen=yes';
+
+                // Membuka jendela baru
+                var w = window.open("https://s.shopee.co.id/3Aq7OcqIox", 'window', params);
+                if (w) {
+                    w.blur(); // Blur jendela baru untuk mencegah pengalihan fokus langsung
+                    setCookie("bkc", "lykshoptinhoc", 20); // Menyimpan cookie yang berlaku selama 20 jam
+                    window.focus(); // Membawa fokus kembali ke jendela utama
+                }
+            }
+        });
+    }, 10000); // Menunda eksekusi selama 10 detik (10.000 ms)
 });
